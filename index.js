d9d80cbcecacb232aa873df2220747a76c081ff0
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2025-09-16 11:32:20 -0300

    feat: creata flow to insert product

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mysql = require('mysql');
 
-const path = require('path');
 const productsRouter = require('./routes/products')
 
 const port = 3000;
 const app = express()
-
 
 const hbs = exphbs.create({
     partialsDir: ['views/partials']
@@ -17,7 +16,7 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
-
 app.use('/products', productsRouter);
+
 
 app.listen(port, () => console.log(`Run on port ${port}`))
--- routes/products.js
+++ routes/products.js
@@ -1,22 +1,63 @@
 const express = require('express')
 const path = require('path');
+const mysql = require('mysql')
 
 const router = express.Router();
 
 const basePath = path.join(__dirname, '../templates');
 
-router.get('/', (req, res) => {
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'estudos'
+});
 
-    const products = [
-        {id: 1, name: 'produto A'},
-        {id: 2, name: 'produto B'},
-        {id: 3, name: 'produto C'},
-    ];
-    res.render('products', {products});
+conn.connect(function (err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Connectou mysql')    
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+    const query = `SELECT * FROM products WHERE id = ${id}`;
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const products = data
+        res.render('products', {products});
+    })
+})
+
+router.get('/', (req, res) => {
+    const query = `SELECT * FROM products`;
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const products = data
+        res.render('products', {products});
+    })
 })
 
 router.post('/', (req, res) => {
-    res.send(req.body)
+    const name  = req.body.name
+    const price = parseFloat(req.body.price)
+    const query = `INSERT INTO products (name, price) VALUES ('${name}', '${price}')`
+    conn.query(query, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/products')
+    })
 });
 
 module.exports = router
